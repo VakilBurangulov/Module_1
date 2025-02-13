95a26a37665037451801b2e0d178460b53cf10ee
Vakil <burangulov.vakil@gmail.com>
2025-02-13 19:35:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Jane";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
