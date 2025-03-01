3474ec2a7fcc660916fb183d1014611473880af5
Vakil <burangulov.vakil@gmail.com>
2025-03-01 13:12:26 +0300

    Echo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,49 +7,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ShowArray(GetArrayFromConsole(10), true);
+            Echo("Привет", 4);
         }
 
-        static int[] GetArrayFromConsole(int num = 5)
+        static void Echo (string msg, int deep)
         {
-            var result = new int[num];
-
-            for (int i = 0; i < result.Length; i++)
+            if ((msg.Length > 2) | (deep > 0))
             {
-                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-                result[i] = int.Parse(Console.ReadLine());
-            }
-
-            return result;
-        }
-
-        static int[] SortArray(int[] array)
-        {
-            int temp;
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[i] < array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-
-            return array;
-        }
-
-        static void ShowArray(int[] array, bool sorted = false)
-        {
-            if (sorted)
-                array = SortArray(array);
-
-            foreach (var item in array)
-            {
-                Console.WriteLine(item);
+                Console.WriteLine(msg);
+                Echo(msg.Remove(0, 2), deep - 1);
             }
         }
     }
